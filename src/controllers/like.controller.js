c9edc4fbbcc9d05ab8abff183d7a1599bb586e2c
Sukanta Behera <sukantabehera096@gmail.com>
2024-01-16 09:49:26 +0530

    add toggle video like end point

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,10 +3,38 @@ import { Like } from "../models/like.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponce } from "../utils/apiResponce.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: toggle like on video
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not exist");
+  }
+
+  const like = await Like.find({
+    $and: [{ likedBy: req.user._id }, { video: video._id }],
+  });
+
+  if (like) {
+    const deletedData = await Like.deleteOne({ _id: like._id });
+
+    return res
+      .status(200)
+      .json(new ApiResponce(200, deletedData, "Video unliked sucessfullu"));
+  } else {
+    const newLike = Like.create({
+      likedBy: req.user._id,
+      video: video._id,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponce(200, newLike, "Video liked sucessfully"));
+  }
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
