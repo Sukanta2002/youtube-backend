f88217cd987069c383a3f25d399aa78153d6c266
Sukanta Behera <sukantabehera096@gmail.com>
2024-01-18 10:05:56 +0530

    add end point for comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,6 +3,7 @@ import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponce } from "../utils/apiResponce.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
@@ -11,8 +12,29 @@ const getVideoComments = asyncHandler(async (req, res) => {
 });
 
 const addComment = asyncHandler(async (req, res) => {
-  // TODO: add a comment to a video
-  
+  // TODO: add a comment to a video testing is required
+
+  const { content, videoId } = req.body;
+
+  if (!content && !videoId) {
+    throw new ApiError(402, "Content and video is missing");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: video._id,
+    owner: req.user._id,
+  });
+
+  res
+    .status(200)
+    .json(new ApiResponce(200, comment, "Comment added sucesfully"));
 });
 
 const updateComment = asyncHandler(async (req, res) => {
